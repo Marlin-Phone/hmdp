de96ecc9728a738395bb4eae4363ebe565ca36f9
Marlin <Marlin_Phone@outlook.com>
2026-01-24 21:55:39 +0800

    feat: 更新用户登录与拦截器功能，支持 Redis 存储与 token 刷新

--- src/main/java/com/hmdp/utils/LoginInterceptor.java
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -1,31 +1,32 @@
 package com.hmdp.utils;
 
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.User;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_TTL;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // 1. 获取session
-        HttpSession session = request.getSession();
-        // 2. 获取用户信息
-        Object user = session.getAttribute("user");
-        // 3. 判断用户是否存在
-        if(user == null){
-            // 4. 不存在,拦截
+        // 1. 判断是否需要拦截（ThreadLocal中是否有用户）
+        if (UserHolder.getUser() == null) {
+            // 2. 没有，拦截，返回401状态码
             response.setStatus(401);
             return false;
         }
-        // 5. 存在，保存用户信息到ThreadLocal
-        UserHolder.saveUser((UserDTO) user);
-        // 6. 放行
+        // 有用户，则放行
         return true;
     }
 
