eaa2bb0826b5a4396940ca97ee92c1c65a4a9158
Marlin <Marlin_Phone@outlook.com>
2026-01-30 22:27:58 +0800

    feat: 添加 RedisIdWorker 类以生成唯一 ID，优化并发处理

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -2,11 +2,16 @@ package com.hmdp;
 
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.method.support.CompositeUriComponentsContributor;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -18,6 +23,12 @@ class HmDianPingApplicationTests {
     private ShopServiceImpl shopServiceImpl;
     @Resource
     CacheClient cacheClient;
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private static final ExecutorService es = Executors.newFixedThreadPool(500);
+    @Autowired
+    private CompositeUriComponentsContributor compositeUriComponentsContributor;
 
     /**
      * 缓存击穿中逻辑过期的预热（店铺id）
@@ -28,4 +39,23 @@ class HmDianPingApplicationTests {
         cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY + 1, shop, 30L, TimeUnit.SECONDS);
     }
 
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                long id = redisIdWorker.nextId("order");
+                System.out.println("id = " + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
 }
