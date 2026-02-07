44cf36622fb12bcf77cdd31afd81ac479cf3cdaf
Marlin <Marlin_Phone@outlook.com>
2026-02-07 18:47:55 +0800

    feat: 添加店铺数据加载功能，支持将店铺信息按类型分组并写入Redis

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,21 +1,33 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
+import com.hmdp.service.IShopService;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisIdWorker;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.method.support.CompositeUriComponentsContributor;
 
 import javax.annotation.Resource;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
+@Slf4j
 @SpringBootTest
 class HmDianPingApplicationTests {
 
@@ -25,10 +37,13 @@ class HmDianPingApplicationTests {
     CacheClient cacheClient;
     @Resource
     private RedisIdWorker redisIdWorker;
-
     private static final ExecutorService es = Executors.newFixedThreadPool(500);
     @Autowired
     private CompositeUriComponentsContributor compositeUriComponentsContributor;
+    @Autowired
+    private IShopService shopService;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 缓存击穿中逻辑过期的预热（店铺id）
@@ -57,5 +72,31 @@ class HmDianPingApplicationTests {
         latch.await();
         long end = System.currentTimeMillis();
         System.out.println("time = " + (end - begin));
+    }
+
+    @Test
+    void loadShopData(){
+        // 1. 查询店铺信息
+        List<Shop> list = shopService.list();
+        // 2. 把店铺分组，按照typeId分组，typeId相同的放在一个集合
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 3. 把分好组的店铺信息写入Redis
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            // 4. 获取类型id
+            Long typeId = entry.getKey();
+            String key = "shop:geo:" + typeId;
+            // 5. 获取同类型的店铺集合
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            // 6. 写入Redis GEOADD key 经度 纬度 member
+            for (Shop shop : value) {
+//                stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                locations.add(new RedisGeoCommands.GeoLocation<>(
+                        shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())
+                ));
+            }
+            stringRedisTemplate.opsForGeo().add(key, locations);
+        }
     }
 }
